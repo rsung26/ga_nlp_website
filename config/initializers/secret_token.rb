438cebf6c73fbae7f08d429ea5bff50c97443a52
rsung26 <rsung26@gmail.com>
2014-01-13 14:05:43 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GaNlpWebsite::Application.config.secret_token = '9a5f86a973f873925e00bdc5adecbcedd82cffe4ef18572a5cf240bb4c68c17a4112e0c25cfac3f905f06496db85df2a0d208f4de9b02f3ffbecff07bd85c165'
